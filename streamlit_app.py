d887d5ca4d07da00a3bf0c5a6e2971fb066fcb6c
hisean4u <120057870+hisean4u@users.noreply.github.com>
2022-12-07 14:40:43 -0600

    Update streamlit_app.py
    Updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 
 import streamlit
 streamlit.title('My parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('kale,spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
